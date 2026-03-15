cea8c0c36c861ddb446e5ac1d62a391412a8b6de
bima1313 <bimairfy13@gmail.com>
2026-03-15 20:51:51 +0700

    feat: add refresh route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import customerRoute from "./customer/index.ts";
 import { AuthRouter } from "./auth.route.ts";
 import { HealthRouter } from "./health.route.ts";
 import adminRoute from "./admin/index.ts";
+import { RefreshRoute } from "./refresh.route.ts";
 
 const registerRoutes: Router = Router();
 
 // Public Route
 registerRoutes.use("/health", HealthRouter);
 registerRoutes.use("/auth", AuthRouter);
+registerRoutes.use("/refresh", RefreshRoute);
 // Protected Route
 registerRoutes.use("/admin", adminRoute);
 registerRoutes.use(customerRoute);
---
+++ src/routes/refresh.route.ts
@@ -0,0 +1,67 @@
+import {
+  Router,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import { prisma } from "../utils/prisma.ts";
+import jwt from "jsonwebtoken";
+import { configuration } from "../config/config.ts";
+
+export const RefreshRoute: Router = Router();
+
+RefreshRoute.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const authHeader = req.headers.authorization;
+      const token = authHeader && authHeader.split(" ")[1];
+
+      if (!token) {
+        res.status(401).json({ message: "Access denied. Token not found" });
+        return;
+      }
+      const user = await prisma.user.findFirst({
+        where: { refreshToken: token },
+      });
+      if (user) {
+        const payload = {
+          id: user.id,
+          name: user.name,
+          role: user.role,
+        };
+        const generateAccessToken = jwt.sign(
+          payload,
+          configuration.jwt.secret,
+          {
+            expiresIn: configuration.jwt.accessExpired,
+          },
+        );
+        const generateRefreshToken = jwt.sign(
+          payload,
+          configuration.jwt.secret,
+          {
+            expiresIn: configuration.jwt.refreshExpired,
+          },
+        );
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { refreshToken: generateRefreshToken },
+        });
+        return res.status(200).json({
+          data: [
+            {
+              message: "success",
+              access_token: generateAccessToken,
+              refresh_token: generateRefreshToken,
+            },
+          ],
+        });
+      } else {
+        res.status(403).json({ message: "Token is invalid or expired." });
+      }
+    } catch (error) {
+      res.status(403).json({ message: "Token is invalid or expired." });
+    }
+  },
+);
